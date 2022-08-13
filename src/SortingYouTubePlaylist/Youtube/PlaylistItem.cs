855f471b475e78d2d75aceb434096ab0c95a246b
dtarroz <damien.tarroz.apps@gmail.com>
2022-08-13 08:36:53 +0200

    fix: suppress warning

--- src/SortingYouTubePlaylist/Youtube/PlaylistItem.cs
+++ src/SortingYouTubePlaylist/Youtube/PlaylistItem.cs
@@ -2,22 +2,22 @@ namespace SortingYouTubePlaylist;
 
 internal sealed class PlaylistItem
 {
-    public PlaylistItemId Id { get; set; }
-    public PlaylistItemVideo Video { get; set; }
+    public PlaylistItemId Id { get; set; } = null!;
+    public PlaylistItemVideo Video { get; set; } = null!;
     public DateTime PublishedAt { get; set; }
     public long Position { get; set; }
 }
 
 internal sealed class PlaylistItemId
 {
-    public string ItemId { get; set; }
-    public string PlaylistId { get; set; }
-    public string Kind { get; set; }
-    public string VideoId { get; set; }
+    public string ItemId { get; set; } = null!;
+    public string PlaylistId { get; set; } = null!;
+    public string Kind { get; set; } = null!;
+    public string VideoId { get; set; } = null!;
 }
 
 internal sealed class PlaylistItemVideo
 {
-    public string ChannelId { get; set; }
+    public string ChannelId { get; set; } = null!;
     public int Duration { get; set; }
 }
